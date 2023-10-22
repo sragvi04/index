b1d844c3f8d0a6f9f768736b1371acb5533052d3
sragvi04 <148708653+sragvi04@users.noreply.github.com>
2023-10-22 22:04:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function circlemouse(){
+    window.addEventListener("mousemove",function(dets){
+        document.querySelector("#minicircle").style.transform=`translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+circlemouse();
